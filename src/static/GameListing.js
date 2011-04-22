dd968ffe1af222c3dabc67083c0f35b7161287fb
Donnie Tolbert <donniet@gmail.com>
2011-04-21 22:56:01 -0500

    #12 simplified the game listing javascript, added columns

--- src/static/GameListing.js
+++ src/static/GameListing.js
@@ -35,29 +35,108 @@ Date.prototype.format = function(format) {
 Date.prototype.toJSON = function(formatter) {
 	return this.toISOString();
 }
-
+/*
 Object.prototype.toJSON = function(formatter) {
 	return this.toString();
+}
+*/
+/*
+ * @author Donnie
+ * @desc binary search through a sorted array
+ * @param val value to search for
+ * @param comp optional comparer func (returns -1, 0, 1)
+ */
+Array.prototype.binarySearch = function(val, comp) {
+	if(typeof comp == "undefined") comp = function(a,b) { if(a<b) return -1; else if(b<a) return 1; else return 0; };
+	
+	var m = 0;
+	var M = this.length - 1;
+	var c = 0;
+	
+	while(m <= M) {
+		var i = Math.floor((M+m) / 2);
+		c = comp(val, this[i]);
+		if(c == 0) return i;
+		if(c < 0) M = i-1;
+		if(c > 0) m = i+1;
+	}
+	if(c < 0) return M;
+	else return m;
 }
 
 function evalScope(str, obj) {
 	var o = new Evalable();
 	for(var k in obj) {
 		o[k] = obj[k];
+		console.log(obj[k]);
 	}
 	
 	return o.eval(str);
 }
 
-function Column(heading, valueBinding) {
-	this.heading_ = heading;
+function Column(valueBinding) {
+	this.heading_ = null;
 	this.valueBinding_ = valueBinding;
-	this.displayBinding_ = null;
+	this.displayBinding_ = null; // defaults to value binding
+	this.groupBinding_ = null;
 	this.sortable_ = false;
+	this.sortField_ = null;
 	this.sortGroups_ = new Array();
+	this.sortGroupNames_ = new Array();
 	this.colindex_ = -1;
+	this.listing_ = null;
+	this.sortDescending_ = false;
 }
-Column.prototype.evalColumn = function(row, rowindex, binding) {
+Column.prototype.setGroups = function(groups) {
+	this.sortGroups_ = groups;
+	return this;
+}
+Column.prototype.setGroupNames = function(groupNames) {
+	this.sortGroupNames_ = groupNames;
+	return this;
+}
+Column.prototype.setSortable = function(sortable) {
+	this.sortable_ = sortable;
+	return this;
+}
+Column.prototype.setDisplayBinding = function(displayBinding) {
+	this.displayBinding_ = displayBinding;
+	return this;
+}
+Column.prototype.setGroupBinding = function(groupBinding) {
+	this.groupBinding_ = groupBinding;
+	return this;
+}
+Column.prototype.setSortField = function(sortField) {
+	this.sortField_ = sortField;
+	return this;
+}
+Column.prototype.setHeading = function(heading) {
+	this.heading_ = heading;
+	return this;
+}
+Column.prototype.getSortable = function() {
+	return this.sortable_;
+}
+Column.prototype.getDisplayBinding = function() {
+	return this.displayBinding_;
+}
+Column.prototype.getGroupBinding = function() {
+	return this.groupBinding_;
+}
+Column.prototype.getSortField = function() {
+	return this.sortField_;
+}
+Column.prototype.getHeading = function() {
+	return this.heading_;
+}
+Column.prototype.sort = function(descending) {
+	if(typeof descending == "undefined")
+		this.sortDescending_ = !this.sortDescending_;
+	else this.sortDescending_ = descending;
+	this.listing_.sortResults(this.colindex_, this.sortDescending_);
+}
+Column.prototype.evalData = function(row, rowindex, binding) {
 	var p = {
 		data: row,
 		col: this.colindex_,
@@ -73,36 +152,246 @@ Column.prototype.evalColumn = function(row, rowindex, binding) {
 	}
 	else return null;
 }
+Column.prototype.value = function(row, rowindex) {
+	return this.evalData(row, rowindex, this.valueBinding_);	
+}
+Column.prototype.display = function(row, rowindex) {
+	return this.evalData(row, rowindex, this.displayBinding_ ? this.displayBinding_ : this.valueBinding_);	
+}
+Column.prototype.group = function(row, rowindex) {
+	return this.evalData(row, rowindex, this.groupBinding_ ? this.groupBinding_ : this.valueBinding_);
+}
+Column.prototype.groupName = function(row, rowindex) {
+	if(this.sortGroupNames_.length == 0) return;
+	
+	var group = this.group(row, rowindex);
+	
+	var index = this.sortGroups_.binarySearch(group);
+	if(index < 0) index = 0;
+	if(index >= this.sortGroupNames_.length) index = this.sortGroupNames_.length - 1;
+	
+	return this.sortGroupNames_[index];
+	
+}
+Column.prototype.renderHeading = function(el) {
+	if(this.heading_ == null) return;
+	
+	var self = this;
+	
+	var txt = document.createTextNode(this.heading_);
+	if(this.sortable_) {
+		var a = document.createElement("a");
+		a.onclick = function() { self.sort(); };
+		a.href = "javascript:void(0)";
+		a.appendChild(txt);
+		el.appendChild(a);
+	}
+	else {
+		el.appendChild(txt);
+	}
+}
+Column.prototype.renderGroup = function(el, row, rowindex) {
+	var ng = this.groupName(row, rowindex);
+	
+	var trg = document.createElement("tr");
+	var tdg = document.createElement("td");
+	tdg.setAttribute("colspan", this.listing_.columns_.length); 
+	tdg.setAttribute("class", "group");
+	if(typeof ng == "string")
+		tdg.appendChild(document.createTextNode(ng));
+	else
+		tdg.appendChild(ng);
+	trg.appendChild(tdg);
+	el.appendChild(trg);
+}
+Column.prototype.render = function(el, row, rowindex) {
+	var disp = this.display(row, rowindex);
+	if(typeof disp == "string")
+		el.appendChild(document.createTextNode(disp));
+	else
+		//HACK: this should at least be in a try/catch
+		el.appendChild(disp);
+}
+
+function ActionsColumn(playersBinding, joinBinding, viewBinding) {
+	Column.apply(this, [playersBinding]);
+	this.playersBinding_ = playersBinding;
+	this.joinBinding_ = joinBinding;
+	this.viewBinding_ = viewBinding;
+}
+ActionsColumn.prototype = new Column();
+ActionsColumn.setSortable = function(sortable) {
+	if(sortable) throw "ActionsColumns cannot be sorted.";
+}
+ActionsColumn.prototype.players = function(row, rowindex) {
+	return this.evalData(row, rowindex, this.playersBinding_);
+}
+ActionsColumn.prototype.joinUrl = function(row, rowindex) {
+	return this.evalData(row, rowindex, this.joinBinding_);
+}
+ActionsColumn.prototype.viewUrl = function(row, rowindex) {
+	return this.evalData(row, rowindex, this.viewBinding_);
+}
+ActionsColumn.prototype.render = function(el, row, rowindex) {
+	//var value = this.value(row, rowindex);
+	var players = this.players(row, rowindex);
+	var joinUrl = this.joinUrl(row, rowindex);
+	var viewUrl = this.viewUrl(row, rowindex);
+	
+	var ul = document.createElement("ul");
+	var li = document.createElement("li");
+	
+	var joined = false;
+	for(var i = 0; !joined && i < players.length; i++) {
+		var p = players[i];
+		if(p.user.email == this.listing_.userEmail_) {
+			li.appendChild(document.createTextNode("joined"));
+			joined = true;
+		}
+	}
+	
+	if(!joined) {
+		var a = document.createElement("a");
+		a.appendChild(document.createTextNode("join"));
+		//TODO: remove hyperlink structure
+		a.href = joinUrl;
+		li.appendChild(a);
+	}
+	
+	ul.appendChild(li);
+	
+	li = document.createElement("li");
+		
+	var a = document.createElement("a");
+	a.appendChild(document.createTextNode("view"));
+	a.href = viewUrl;
+	li.appendChild(a);
+	
+	ul.appendChild(li);
+	
+	el.appendChild(ul);
+}
+
+function PlayersColumn(valueBinding) {
+	Column.apply(this, [valueBinding]);
+}
+PlayersColumn.prototype = new Column();
+PlayersColumn.setSortable = function(sortable) {
+	if(sortable) throw "PlayersColumns cannot be sorted.";
+}
+PlayersColumn.prototype.render = function(el, row, rowindex) {
+	var ul = document.createElement("ul");
+	var players = this.value(row, rowindex);
+	
+	for(var i = 0; i < players.length; i++) {
+		var p = players[i];
+		var li = document.createElement("li");
+		li.className = "player-" + p.color;
+		
+		var a = document.createElement("a");
+		//TODO: escape the email
+		a.href = "/player?email=" + p.user.email;
+		a.appendChild(document.createTextNode(p.user.nickname));
+		li.appendChild(a);
+		
+		li.appendChild(document.createTextNode(": " + p.score));
+		
+		ul.appendChild(li); 
+	}
+	el.appendChild(ul);
+}
+
+function GameListingService(url) {
+	this.url_ = url;
+}
+
+/*
+ * @author Donnie
+ * @desc Queries the boards while sorting and filtering.
+ * @param sorts Array of {field,desc} objects where desc refers to descending
+ * @param filters Array of {field,value,op} objects where op is =,!=,<,>,<=,>=
+ * @param offset is an integer offset of the records
+ * @param limit is the record limit
+ */
+GameListingService.prototype.queryBoards = function(offset, limit, sorts, filters) {
+	var sort = "";
+	for(var i = 0; sorts && i < sorts.length; i++) {
+		console.log("sorts");
+		var s = sorts[i];
+		if(s.desc) sort += "-" + s.field;
+		else sort += s.field;
+		
+		if(i < sorts.length - 1) sort += ",";
+	}
+	var filter = "";
+	for(var i = 0; filters && i < filters.length; i++) {
+		console.log("filters");
+		var f = filters[i];
+		//TODO: json encode value
+		filter += f.field + (f.op ? f.op : "=") + f.value;
+	}
+	
+	var responder = new Object();
+	
+	this._execute({
+			sorts: sort,
+			filter: filter,
+			limit: limit,
+			offset: offset
+		},
+		responder
+	);
+	
+	return responder;
+}
+GameListingService.prototype._execute = function(params, responder) {
+	var self = this;
+	$.ajax({url: this.url_, data: $.param(params), dataType: "text", type: "GET",
+	    success: function(data) {
+	    	//TODO: read in the JSON here...
+	    	Event.fire(responder, "load", [eval("(" + data + ")")]);
+		}, 
+		error: function(response) {
+			Event.fire(responder, "error", [response]);
+			console.log("ERROR:", response);
+		}
+	});
+}
+
 
 function GameListing(userEmail, url) {
 	this.userEmail_ = userEmail;
 
 	this.url_ = url;
+	this.service_ = new GameListingService(this.url_);
 	this.el_ = null;
 	this.tbody_ = null;
 	
 	//TODO: I don't like the binding mechanism.
 	this.columns_ = [
+	    /*
 		{heading:"Game Key", binding: "GameListing.anchorBinding(this.data.gameKey, '/game/'+this.data.gameKey+'/')", value:"this.data.gameKey", field:"gameKey", sortable:true, className:"gameKey"},
 		{heading:"Owner", binding: "this.data.owner.nickname", value:"this.data.owner", field:"owner", sortable:true, className:"owner"},
 		{heading:"Created", grouping:GameListing.dateGrouping, value:"this.data.dateTimeCreated", binding: "Date.parseISO(this.data.dateTimeCreated).format()", field:"dateTimeCreated", sortable:true, className:"created"},
 		{heading:"Ended", grouping:GameListing.dateGrouping, value:"this.data.dateTimeEnded", binding:"this.data.dateTimeEnded ? Date.parseISO(this.data.dateTimeEnded).format() : 'ongoing'", field:"dateTimeEnded", sortable:true},
 		{heading:"Players", binding: this.playerBinding, value:"this.data.players", field:"players", sortable:false }, 
 		{heading:"", binding: this.joinBinding, sortable: false, className: "commands"}
+		*/
 	];
 	
 	this.sortBy_ = -1;
-	this.sortDir_ = GameListing.SortDir.Ascending;
 	this.sorts_ = [];
 	this.filters_ = [];
 	this.limit_ = 100;
 	this.offset_ = 0;
 }
 GameListing.prototype.addColumn = function(column) {
+	var index = this.columns_.length;
+	column.colindex_ = index;
+	column.listing_ = this;
 	this.columns_.push(column);
-	return this.columns_.length - 1;
+	return index;
 }
-GameListing.SortDir = {Ascending:"ASC", Descending:"DESC"};
 GameListing.anchorBinding = function(text, href, opts) {
 	var a = document.createElement("a");
 	a.href = href;
@@ -160,71 +449,6 @@ GameListing.prototype.setLimit = function(limit) {
 		this.refresh();
 	}
 }
-GameListing.prototype.playerBinding = function(el, data, coldef, row, column, gameListing) {
-	var ul = document.createElement("ul");
-	for(var i = 0; i < data.players.length; i++) {
-		var p = data.players[i];
-		var li = document.createElement("li");
-		li.className = "player-" + p.color;
-		
-		var a = document.createElement("a");
-		//TODO: escape the email
-		a.href = "/player?email=" + p.user.email;
-		a.appendChild(document.createTextNode(p.user.nickname));
-		li.appendChild(a);
-		
-		li.appendChild(document.createTextNode(": " + p.score));
-		
-		ul.appendChild(li); 
-	}
-	el.appendChild(ul);
-}
-GameListing.prototype.joinBinding = function(el, data, coldef, row, column, gameListing) {
-	var ul = document.createElement("ul");
-	
-	var li = document.createElement("li");
-	
-	var joined = false;
-	for(var i = 0; !joined && i < data.players.length; i++) {
-		var p = data.players[i];
-		if(p.user.email == gameListing.userEmail_) {
-			li.appendChild(document.createTextNode("joined"));
-			joined = true;
-		}
-	}
-	
-	if(!joined) {
-		var a = document.createElement("a");
-		a.appendChild(document.createTextNode("join"));
-		//TODO: remove hyperlink structure
-		a.href = "/game/" + data.gameKey + "/join";
-		li.appendChild(a);
-	}
-	
-	ul.appendChild(li);
-	
-	li = document.createElement("li");
-		
-	var a = document.createElement("a");
-	a.appendChild(document.createTextNode("view"));
-	a.href = "/game/" + data.gameKey + "/";
-	li.appendChild(a);
-	
-	ul.appendChild(li);
-	
-	el.appendChild(ul);
-}
-GameListing.prototype.createdBinding = function(el, data, coldef, row, column) {
-	
-	var txt = document.createTextNode(formatDate(data.dateTimeCreated));
-	el.appendChild(txt);
-}
-GameListing.prototype.gameKeyBinding = function(el, data, coldef, row, column) {
-	var a = document.createElement("a");
-	a.appendChild(document.createTextNode(data.gameKey));
-	a.href = "/game/" + data.gameKey + "/";
-	el.appendChild(a);
-}
 GameListing.prototype.handleResults = function(results) {
 	for(var i = this.tbody_.childNodes.length - 1; i >= 0; i--) {
 		this.tbody_.removeChild(this.tbody_.childNodes[i]);
@@ -239,56 +463,19 @@ GameListing.prototype.handleResults = function(results) {
 			var col = this.columns_[j];
 			var td = document.createElement("td");
 			
-			var obj = new Evalable();
-			obj.data = r;
-			obj.col = j;
-			obj.row = i;
-			obj.coldata = col;
-			
-			var value = null;
-			
-			if(typeof col.value == "string") {
-				value = obj.eval(col.value);
-			}
-			else if(typeof col.value == "function") {
-				value = col.value(r);
-			}
+			var value = col.value(r, i);
+			var display = col.display(r, i);
 			
 			if(this.sortBy_ == j) {
-				var ng = null;
-				if(typeof col.grouping == "string") {
-					ng = obj.eval(col.grouping);
-				}
-				else if(typeof col.grouping == "function") {
-					ng = col.grouping(value, r, results);
-				}
+				var ng = col.groupName(r, i);
 				
 				if(ng != null && ng != group) {
-					var trg = document.createElement("tr");
-					var tdg = document.createElement("td");
-					tdg.setAttribute("colspan", this.columns_.length); 
-					tdg.setAttribute("class", "group");
-					tdg.appendChild(document.createTextNode(ng));
-					trg.appendChild(tdg);
-					this.tbody_.appendChild(trg);
+					col.renderGroup(this.tbody_, r, i);
 					group = ng;
 				}
 			}
 			
-			if(typeof col.binding == "function") {
-				
-				col.binding.apply(r, [td, r, col, i, j, this]);
-			}
-			else {
-				
-				var el = obj.eval(col.binding);
-				
-				if(typeof el == "string")
-					td.appendChild(document.createTextNode(el));
-				else 
-					td.appendChild(el);
-					
-			}
+			col.render(td, r, i);
 			
 			tr.appendChild(td);
 		}
@@ -297,31 +484,6 @@ GameListing.prototype.handleResults = function(results) {
 };
 GameListing.prototype.addFilter = function(field, value, op) {
 	this.filters_.push({field:field, value:value, op:op});
-}
-GameListing.prototype.createParams = function() {
-	var sort = "";
-	console.log("about to sort" + this.sorts_.length);
-	for(var i = 0; i < this.sorts_.length; i++) {
-		console.log("sorts");
-		var s = this.sorts_[i];
-		if(s.desc) sort += "-" + s.field;
-		else sort += s.field;
-		
-		if(i < this.sorts_.length - 1) sort += ",";
-	}
-	var filter = "";
-	for(var i = 0; i < this.filters_.length; i++) {
-		console.log("filters");
-		var f = this.filters_[i];
-		//TODO: json encode value
-		filter += f.field + (f.op ? f.op : "=") + f.value;
-	}
-	return {
-		sorts: sort,
-		filter: filter,
-		limit: this.limit_,
-		offset: this.offset_
-	};
 }
 GameListing.prototype.render = function(el) {
 	var self = this;
@@ -343,19 +505,8 @@ GameListing.prototype.render = function(el) {
 				
 		var th = document.createElement("th");
 		
-		var txt = document.createTextNode(col.heading);
-		if(col.sortable) {
-			var a = document.createElement("a");
-			a.onclick = function(colindex) {
-				return function() { self.handleSort(colindex); };
-			}(i);
-			a.href = "javascript:void(0)";
-			a.appendChild(txt);
-			th.appendChild(a);
-		}
-		else {
-			th.appendChild(txt);
-		}
+		col.renderHeading(th);
+		
 		tr.appendChild(th);
 	}
 	thead.appendChild(tr);
@@ -369,21 +520,14 @@ GameListing.prototype.render = function(el) {
 	
 	this.refresh();
 }
-GameListing.prototype.sortResults = function(sortby, ascending) {
+GameListing.prototype.sortResults = function(sortby, descending) {
 	if(sortby < 0 || sortby >= this.columns_.length) {
 		throw "sortResults: sortby index out of bounds";
 	}
 	this.sortBy_ = sortby;
-	
-	if(typeof ascending == "undefined") {
-		this.sortDir_ = (this.sortDir_ == GameListing.SortDir.Ascending) ? GameListing.SortDir.Descending : GameListing.SortDir.Ascending;
-	}
-	else {
-		this.sortDir_ = ascending ? GameListing.SortDir.Ascending : GameListing.SortDir.Descending;
-	}
-	
+		
 	var col = this.columns_[this.sortBy_];
-	this.sorts_ = [{field: col.field, desc: (this.sortDir_ == GameListing.SortDir.Ascending)}];
+	this.sorts_ = [{field: col.getSortField(), desc: !descending}];
 		
 	if(this.el_) this.refresh();
 }
@@ -392,14 +536,13 @@ GameListing.prototype.handleSort = function(colindex) {
 };
 GameListing.prototype.refresh = function() {
 	var self = this;
-	$.ajax({url: this.url_, data: $.param(this.createParams()), dataType: "text", type: "GET",
-        success: function(data) {
-        	//TODO: read in the JSON here...
-			self.handleResults(eval("(" + data + ")"));
-		}, 
-		error: function(response) {
-    		console.log("ERROR:", response);
-		}
-    });
+	
+	var responder = this.service_.queryBoards(this.offset_, this.limit_, this.sorts_, this.filters_);
+	
+	Event.addListener(responder, "load", this.handleResults, this);
+	
+	Event.addListener(responder, "error", function(message) {
+		//TODO: handle service error
+	});
     
 }
